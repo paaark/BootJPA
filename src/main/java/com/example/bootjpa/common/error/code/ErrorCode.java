b1e1d2b2971e707e0b157fe4235c597100bd2370
paaark <blockdaon_park@naver.com>
2021-07-20 21:19:39 +0900

    Domain Add Post

--- src/main/java/com/example/bootjpa/common/error/code/ErrorCode.java
+++ src/main/java/com/example/bootjpa/common/error/code/ErrorCode.java
@@ -7,7 +7,11 @@ public enum ErrorCode {
 
     NOT_FOUND_EMAIL(400, "A004001", "이메일을 찾을 수 없습니다."),
     DUPLICATE_EMAIL(400,"A004002","중복된 이메일입니다."),
-    NOT_EQUAL_PASSWORD(400,"A004003","비밀번호가 일치하지 않습니다.");
+    NOT_EQUAL_PASSWORD(400,"A004003","비밀번호가 일치하지 않습니다."),
+
+    NOT_FOUND_POST(400, "P004001", "게시글을 찾을 수 없습니다."),
+    NOT_TYPE_SUBJECT(400, "P004002", "제목을 입력해주세요."),
+    NOT_TYPE_CONTENT(400, "P004003", "내용을 입력해주세요.");
 
 
     private int status;
